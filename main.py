f419361185737c6f823ca007aef75fefd1ff7f8d
Matheus <matheusc1@al.insper.edu.br>
2023-11-24 09:06:46 -0300

    metodos aplicados

--- main.py
+++ main.py
@@ -11,9 +11,22 @@ KG = calcula_KG(lista_KE, Inc)
 
 KG_com_restricoes = CondicoesContorno_KG(KG, R)
 F_com_restricoes = CondicoesContorno_F(F, R)
-print(F)
+#print(F)
 #print(KG)
 #print(R)
 #print(nr)
 #print(KG_com_restricoes)
-print(F_com_restricoes)
+#print(F_com_restricoes)
+
+#Tolerancias usadas em ambas foi recomendada pelo professor
+jacobi_feito = jacobi(100, 10e-10, KG_com_restricoes, F_com_restricoes)
+print(jacobi_feito)
+print("///////////////////////")
+
+# Vamos usar o gauss_seidel porque o resultado dele possui menos iterações e porque 
+gauss_seidel_feito = gauss_seidel(100, 10e-10, KG_com_restricoes, F_com_restricoes)
+print(gauss_seidel_feito)
+print("///////////////////////")
+
+controle = np.linalg.solve(KG_com_restricoes, F_com_restricoes)
+print(controle)
